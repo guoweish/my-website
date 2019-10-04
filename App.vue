f59e1e2c33a2182a4e19e09f63290f484355f894
guoweish <guoweish@163.com>
2019-10-04 20:41:31 +0800

    draft

---
+++ App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id='app'>
+    <ul>
+      <li v-for="proj in instanceData" :key="proj.id">
+        <!-- <div>{{proj.name}}</div> -->
+        <a v-bind:href="proj.url" target="_blank">{{proj.name}}</a>
+      </li>
+    </ul>
+
+    <HeadComp></HeadComp>
+
+    <h1>
+      {{greeting}}
+    </h1>
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue';
+  import HeadComp from './Head.vue';
+
+  export default {
+    data() {
+      return {
+        greeting: "hello world!",
+        instanceData: [
+          {
+            id: 1,
+            name: "abcdefg",
+            url: '/projects/proj-noise/'
+          },
+          {
+            id: 2,
+            name: "hey guys",
+            url: '/projects/proj-noise/'
+          }
+        ]
+      }
+    },
+    components: {
+      HeadComp
+    }
+  }
+</script>
+
+<style>
+  body {
+    /* background-color: black; */
+    margin: 0px;
+  }
+</style>
---
+++ Head.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id='head'>
+    <h1>
+      {{headName}}
+    </h1>
+  </div>
+</template>
+
+<script>
+  // import Vue from 'vue';
+
+  export default {
+    data() {
+      return {
+        headName: "my blog"
+      }
+    }
+  }
+
+  // var instanceData = {
+  //   projectsList: [
+  //     {
+  //       id: 'a',
+  //       url: 'xxx'
+  //     },
+  //     {
+  //       id: 'b',
+  //       url: 'xxx'
+  //     }
+  //   ]
+  // }
+
+  // new Vue({
+  //   el: '#app',
+  //   data: instanceData
+  // })
+</script>
+
+<style>
+</style>
