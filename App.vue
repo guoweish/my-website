2ec7c3fe4d1491eec53f84516ba26b56120bd13c
guoweish <guoweish@163.com>
2019-10-05 23:29:41 +0800

    leave shanghai

--- App.vue
+++ App.vue
@@ -3,15 +3,11 @@
     <ul>
       <li v-for="proj in instanceData" :key="proj.id">
         <!-- <div>{{proj.name}}</div> -->
-        <a v-bind:href="proj.url" target="_blank">{{proj.name}}</a>
+        <a v-bind:href="proj.url" target="_blank">{{proj.name}} <img v-bind:src="proj.img" /></a>
       </li>
     </ul>
 
     <HeadComp></HeadComp>
-
-    <h1>
-      {{greeting}}
-    </h1>
   </div>
 </template>
 
@@ -19,23 +15,32 @@
   import Vue from 'vue';
   import HeadComp from './Head.vue';
 
+  import DataConfig from './DataConfig.js';
+  console.log(DataConfig)
+
+  const testImage = require('./statics/github.png');
+
   export default {
     data() {
-      return {
-        greeting: "hello world!",
-        instanceData: [
-          {
-            id: 1,
-            name: "abcdefg",
-            url: '/projects/proj-noise/'
-          },
-          {
-            id: 2,
-            name: "hey guys",
-            url: '/projects/proj-noise/'
-          }
-        ]
-      }
+      // return {
+      //   greeting: "hello world!",
+      //   instanceData: [
+      //     {
+      //       id: 1,
+      //       name: "abcdefg",
+      //       url: '/projects/proj-noise/',
+      //       img: testImage
+      //     },
+      //     {
+      //       id: 2,
+      //       name: "hey guys",
+      //       url: '/projects/proj-noise/',
+      //       img: testImage
+      //     }
+      //   ]
+      // }
+
+      return DataConfig
     },
     components: {
       HeadComp
---
+++ DataConfig.js
@@ -0,0 +1,18 @@
+const testImage = require('./statics/github.png');
+
+export default {
+  instanceData: [
+    {
+      id: 1,
+      name: "abcdefg",
+      url: '/projects/proj-noise/',
+      img: testImage
+    },
+    {
+      id: 2,
+      name: "hey guys",
+      url: '/projects/proj-noise/',
+      img: testImage
+    }
+  ]
+}
